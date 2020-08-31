095d42e3071b70149231993d6c2ae792ff90d5fc
Erikh <domingueserikh@gmail.com>
2020-08-31 10:33:19 -0300

    FieldPlay

--- Snake.c
+++ Snake.c
@@ -1,5 +1,48 @@
 #include <stdio.h>
 
-int main(){
-    
+#define N 20
+#define M 40
+
+int i, j;
+
+void buildFieldPlay() {
+    for(i=0; i<=M+1; i++) {
+        if(i==0){
+            printf("%c", 201);
+        } else if (i==M+1) {
+            printf("%c", 187);
+        }
+        else {
+            printf("%c", 205);
+        }
+    }
+    printf("\n");
+
+    for(i=0; i<N; i++){
+        printf("%c", 186);
+        for(j=0; j<M; j++) {
+            if(j==M-1) {
+                printf(" %c\n", 186);
+            } else {
+                printf(" ");
+            }
+        }
+    }
+
+
+    for(i=0; i<=M+1; i++) {
+        if(i==0){
+            printf("%c", 200);
+        } else if (i==M+1) {
+            printf("%c", 188);
+        }
+        else {
+            printf("%c", 205);
+        }
+    }
+}
+
+
+void main(){
+    buildFieldPlay();
 }
