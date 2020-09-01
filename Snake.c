98e340a74306bf7c0c778b1b65d2d6a2c12dc106
Erikh <domingueserikh@gmail.com>
2020-08-31 22:45:52 -0300

    Add Random Frog

--- Snake.c
+++ Snake.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include <windows.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <time.h>
 
 #define N 20
 #define M 40
 
-int i, j, Field[N][M], x, y, Gy, Head, Tail;
+int i, j, Field[N][M], x, y, Gy, Head, Tail, Game, Frogs, a, b;
 
 void snakeInitialization() {
     for(i=0; i<N; i++) {
@@ -14,9 +18,11 @@ void snakeInitialization() {
 
     x = N/2; 
     y = M/2;
+    Gy = y;
     Head = 5;
     Tail = 1;
-    Gy = y;
+    Game = 0;
+    Frogs = 0;
 
     for(i=0; i<Head; i++) {
         Gy++;
@@ -43,6 +49,7 @@ void buildFieldPlay() {
             if(Field[i][j]==0) printf(" ");
             if(Field[i][j]>0 && Field[i][j] != Head) printf("%c", 176); 
             if(Field[i][j] == Head) printf("%c", 178);
+            if(Field[i][j]==-1) printf("%c", 15);
             if(j==M-1) printf("%c\n", 186);
         }
     }
@@ -53,15 +60,39 @@ void buildFieldPlay() {
             printf("%c", 200);
         } else if (i==M+1) {
             printf("%c", 188);
-        }
-        else {
+        } else {
             printf("%c", 205);
         }
     }
 }
 
+void ResetScreenPosition() {
+    HANDLE hOut;
+    COORD Position;
+    hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    Position.X = 0;
+    Position.Y = 0;
+    SetConsoleCursorPosition(hOut, Position);
+}
+
+Random(){
+	srand(time(0));
+	a = 1 + rand() % 18; 
+	b = 1 + rand() % 38;
+	
+	if(Frogs == 0 && Field[a][b] == 0) {
+		Field[a][b] = -1;
+		Frogs = 1;
+	}
+}
 
 void main(){
     snakeInitialization();
-    buildFieldPlay();
+
+    while(Game == 0) {
+        buildFieldPlay();
+        ResetScreenPosition();
+        Random();
+    }
+
 }
